612a9c3d1b97da4c86039434bae878be0e7fc838
Natalie Nguyen <natalienguyen@sandiego.edu>
2024-03-05 14:40:05 -0800

    added names of editors

--- UniversityGeocaching/UniversityGeocachingApp.swift
+++ UniversityGeocaching/UniversityGeocachingApp.swift
@@ -3,6 +3,7 @@
 //  UniversityGeocaching
 //
 //  Created by Tia Merheb on 2/7/23.
+//  Continued by Maria Varriale Gomez, David Amano, Natalie Nguyen, and Michael Gallagher in Spring 2024
 //
 
 import SwiftUI
