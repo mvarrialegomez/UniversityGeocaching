96620bda7858e841269c46fc57e6ff28057e45e4
Maria Varriale Gomez <mvarrialegomez@sandiego.edu>
2024-04-19 17:20:49 -0700

    Created a layout for the "Cache Status" button within the Admin Page.

--- UniversityGeocaching/Navigation/CacheStatusView.swift
+++ UniversityGeocaching/Navigation/CacheStatusView.swift
@@ -9,60 +9,65 @@ import SwiftUI
 
 struct CacheStatusView: View {
     // Sample data
-    var allCaches = [
-        CacheStatus(title: "USD Torero Store", numClaimed: 3, users: "Maria Varriale Gomez, Natalie Nyugen, David Amano"),
-        CacheStatus(title: "Student Life Pavilion", numClaimed: 1, users: "Michael Gallagher"),
-        CacheStatus(title: "Warren Hall", numClaimed: 0, users: "None"),
-        CacheStatus(title: "Copley Library", numClaimed: 5, users: "Maria Varriale Gomez, Natalie Nyugen, David Amano, Michael Gallagher, Sean Limqueco")
-    ]
-    
-    struct CacheStatus: Identifiable {
-        let title: String
-        let numClaimed: Int
-        let users: String
-        let id = UUID()
-    }
-    
     var body: some View {
         NavigationView {
             VStack {
+                Text("Cache Status")
+                    .font(.system(size: 30))
+                    .bold()
                 
-                VStack {
-                    Text("Cache Status")
-                        .font(.system(size: 30))
-                        .bold()
-                    
-            //        Text("Total caches completed: 3")
-              //          .font(.title2)
-                    //List(allCaches, id: \.title) { CacheStatus in
-
-//                    List(allCaches) {
-//                        VStack(alignment: .leading) {
-//                            Text($0.title)
-//                                .font(.headline)
-//                            Text($0.numCompleted)
-//                                .font(.subheadline)
-//                                .foregroundColor(.secondary)
-//                            Text($0.users)
-//                                .font(.footnote)
-//                                .foregroundColor(.gray)
-//                        }
-//                    }
-                  
-           //         List(allCaches, id: \.title) { CacheStatus in
-
-                    List(allCaches) {
-                        Text($0.title)
-                        //Text($0.numCompleted)
-                        //Text($0.users)
+                Spacer()
+            
+                List {
+                    Section {
+                        DisclosureGroup("USD Torero Store (3)") {
+                            HStack(spacing: 3) {
+                                Label("Maria Varriale Gomez", systemImage: "person.circle.fill")
+                            }
+                            HStack(spacing: 3) {
+                                Label("Natalie Nyugen", systemImage: "person.circle.fill")
+                            }
+                            HStack(spacing: 3) {
+                                Label("David Amano", systemImage: "person.circle.fill")
+                            }
+                        }
+                    }
+                    Section {
+                        DisclosureGroup("Student Life Pavilion (1)") {
+                            HStack(spacing: 3) {
+                                Label("Michael Gallagher", systemImage: "person.circle.fill")
+                            }
+                        }
+                    }
+                    Section {
+                        DisclosureGroup("Warren Hall") {
+                            HStack(spacing: 3) {
+                                Label("None", systemImage: "person.fill.xmark")
+                            }
+                        }
+                    }
+                    Section {
+                        DisclosureGroup("Copley Library (5)") {
+                            HStack(spacing: 3) {
+                                Label("Maria Varriale Gomez", systemImage: "person.circle.fill")
+                            }
+                            HStack(spacing: 3) {
+                                Label("Natalie Nyugen", systemImage: "person.circle.fill")
+                            }
+                            HStack(spacing: 3) {
+                                Label("David Amano", systemImage: "person.circle.fill")
+                            }
+                            HStack(spacing: 3) {
+                                Label("Michael Gallagher", systemImage: "person.circle.fill")
+                            }
+                            HStack(spacing: 3) {
+                                Label("Sean Limqueco", systemImage: "person.circle.fill")
+                            }
+                        }
                     }
                     
-                    
-                    
-                    
-                    
-                }.padding(.top, 50) // Add padding above "Past Quests" - END OF VSTACK
-                
+                }
+
             }
         }
     }
